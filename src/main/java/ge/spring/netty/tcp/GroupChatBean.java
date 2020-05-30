4c86995c6939b920976347fdfe0477966f2f60d0
P-peng <894194651@qq.com>
2020-05-30 09:43:46 +0800

    注释

--- src/main/java/ge/spring/netty/tcp/GroupChatBean.java
+++ src/main/java/ge/spring/netty/tcp/GroupChatBean.java
@@ -7,6 +7,8 @@ import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
 /**
+ *
+ *
  * @author dengzhipeng
  * @version 1.0
  * @date 2020/5/29 0029
@@ -14,6 +16,11 @@ import java.util.concurrent.ConcurrentHashMap;
 @Component
 public class GroupChatBean {
 
+    /**
+     * 该Map装载了所有 tcp的连接
+     * k -> 连接的 channelId
+     * v -> 连接的 channel
+     */
     public Map<String, Channel> channels = new ConcurrentHashMap<>(64);
 
 }
